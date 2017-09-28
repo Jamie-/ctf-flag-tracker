d13eadd754a5de655c323adf79a1e464a60bd655
Jamie Scott <github@jami.org.uk>
2017-09-28 16:39:39 +0100

    FIxed bug made fixing the previous bug

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, NumberRange
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
