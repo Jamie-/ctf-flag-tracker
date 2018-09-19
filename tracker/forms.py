adce7f99c969ae6a32f6bdf974c2cd5070ad44d4
Josh Curry <josh.curry@talktalk.net>
2018-09-19 21:03:02 +0100

    Add development mode to disable recaptcha.

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,7 +1,7 @@
 import flask_wtf
 import wtforms
 import wtforms.validators as validators
-
+import tracker
 
 class LoginForm(flask_wtf.FlaskForm):
     username = wtforms.StringField('username', validators=[validators.DataRequired()])
@@ -13,12 +13,13 @@ class RegisterForm(flask_wtf.FlaskForm):
     name = wtforms.StringField('name', validators=[validators.DataRequired(), validators.Length(min=3, max=50)])
     password = wtforms.PasswordField('password', validators=[validators.DataRequired(), validators.Length(min=8)])
     password2 = wtforms.PasswordField('password2', validators=[validators.DataRequired(), validators.Length(min=8)])
-    recaptcha = flask_wtf.RecaptchaField()
-
+    if not tracker.app.config['DEVELOPMENT_MODE']:
+        recaptcha = flask_wtf.RecaptchaField()
+    else:
+        recaptcha = wtforms.HiddenField('nothing')
 
 class FlagForm(flask_wtf.FlaskForm):
     flag = wtforms.StringField('flag', validators=[validators.DataRequired()])
-
 
 class TeamForm(flask_wtf.FlaskForm):
     team = wtforms.StringField('team', validators=[validators.DataRequired(), validators.Length(3)])
