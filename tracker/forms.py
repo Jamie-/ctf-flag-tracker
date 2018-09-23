038e7ce721222c6707fa12f906015edcf36c982b
Jamie Scott <github@jami.org.uk>
2018-09-23 19:39:10 +0100

    Renamed to DEBUG and added check to stop 500 errors when key missing

--- tracker/forms.py
+++ tracker/forms.py
@@ -3,6 +3,7 @@ import wtforms
 import wtforms.validators as validators
 import tracker
 
+
 class LoginForm(flask_wtf.FlaskForm):
     username = wtforms.StringField('username', validators=[validators.DataRequired()])
     password = wtforms.PasswordField('password', validators=[validators.DataRequired()])
@@ -13,13 +14,15 @@ class RegisterForm(flask_wtf.FlaskForm):
     name = wtforms.StringField('name', validators=[validators.DataRequired(), validators.Length(min=3, max=50)])
     password = wtforms.PasswordField('password', validators=[validators.DataRequired(), validators.Length(min=8)])
     password2 = wtforms.PasswordField('password2', validators=[validators.DataRequired(), validators.Length(min=8)])
-    if not tracker.app.config['DEVELOPMENT_MODE']:
+    if 'DEBUG' in tracker.app.config and not tracker.app.config['DEBUG']:
         recaptcha = flask_wtf.RecaptchaField()
     else:
         recaptcha = wtforms.HiddenField('nothing')
 
+
 class FlagForm(flask_wtf.FlaskForm):
     flag = wtforms.StringField('flag', validators=[validators.DataRequired()])
+
 
 class TeamForm(flask_wtf.FlaskForm):
     team = wtforms.StringField('team', validators=[validators.DataRequired(), validators.Length(3)])
