c4975fef988364488080362e8caf4d80568d4508
Jamie Scott <github@jami.org.uk>
2018-11-12 20:01:06 +0000

    Fixed ip_log bug when using nginx

--- tracker/ip_log.py
+++ tracker/ip_log.py
@@ -9,4 +9,7 @@ logger = logging.getLogger(__name__)
 @app.before_request
 def ip_logging():
     if flask_login.current_user.is_authenticated:
-        logger.info('^%s^ coming from %s, doing %s at %s', flask_login.current_user.get_id(), flask.request.remote_addr, flask.request.method, flask.request.path)
+        ip_addr = flask.request.remote_addr
+        if 'X-Real-IP' in flask.request.headers:  # Detect nginx reverse proxying
+            ip_addr = flask.request.headers['X-Real-IP']
+        logger.info('^%s^ coming from %s, doing %s at %s', flask_login.current_user.get_id(), ip_addr, flask.request.method, flask.request.path)
